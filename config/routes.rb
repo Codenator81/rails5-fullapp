1012e161f101355d6232b5f964fdc116631eb3e8
Codenator <codenator81@gmail.com>
2016-04-18 00:40:31 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  root 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Sample App"
   end
 
   test "should get aboout" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Sample App"
   end
 end
