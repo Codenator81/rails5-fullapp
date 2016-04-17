1702db2aa69e11492f024fa8b9d83b249116a6cd
Codenator <codenator81@gmail.com>
2016-04-18 01:05:40 +0300

    Add guard test and resolve exercise from lesson 3

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get aboout" do
-    get static_pages_about_url
+    get static_pages_about_path
     assert_response :success
-    assert_select "title", "About | Sample App"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
